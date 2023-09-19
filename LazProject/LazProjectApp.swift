dbd4efb5c6ebdaf88b9a2e90a713d4baadbe88ad
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-09-19 11:02:44 +0700

    Initial Commit

---
+++ LazProject/LazProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazProjectApp.swift
+//  LazProject
+//
+//  Created by Putut Yusri Bahtiar on 19/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LazProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

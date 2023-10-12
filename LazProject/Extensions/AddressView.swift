d0e942cf8a57456374a5132b85d42ee0043a9f91
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-10-12 08:37:16 +0700

    commit

---
+++ LazProject/Extensions/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  LazProject
+//
+//  Created by Putut Yusri Bahtiar on 12/10/23.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}

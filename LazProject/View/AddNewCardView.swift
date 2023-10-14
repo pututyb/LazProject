3326b009c40d818817bbb2e237e833112f40d644
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-10-14 18:09:07 +0700

    commit

--- LazProject/View/AddNewCardView.swift
+++ LazProject/View/AddNewCardView.swift
@@ -8,22 +8,57 @@
 import SwiftUI
 
 struct AddNewCardView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var btnImages = ["mastercard", "paypal", "logo"]
+    
+    
+    @State private var selectedButton: String?
+    
     var body: some View {
         VStack {
             HStack {
-                Button(action: {
-                    
-                }) {
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(Color("bgButtonBack"))
-                        .frame(width: 100, height: 50)
-                        .overlay(
-                            Image("mastercard")
-                                .font(.system(size: 26))
-                        )
+                ForEach(btnImages, id: \.self) { imageName in
+                    Button(action: {
+                        selectedButton = imageName
+                    }) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundStyle(selectedButton == imageName ? Color("roseWhite") : Color("bgButtonBack"))
+                            .frame(width: 100, height: 50)
+                            .overlay(
+                                Image(imageName)
+                                    .font(.system(size: 26))
+                            )
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(selectedButton == imageName ? Color.orange : Color.clear, lineWidth: 2)
+                            )
+                            .padding(.horizontal)
+                    }
                 }
             }
-        }
+            .padding(.top)
+            
+            Spacer()
+        }.navigationBarBackButtonHidden(true)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Circle()
+                            .foregroundColor(Color("bgButtonBack"))
+                            .frame(width: 50, height: 50)
+                            .overlay(
+                                Image(systemName: "arrow.left")
+                                    .foregroundColor(Color.primary)
+                            )
+                    }
+                }
+            }
+            .padding(.top)
+            .navigationTitle("Add New Card")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 

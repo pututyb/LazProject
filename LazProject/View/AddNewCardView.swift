f0cba1f0cd7408d696a1a703e48c59b37e955ab7
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-10-14 18:45:41 +0700

    commit

--- LazProject/View/AddNewCardView.swift
+++ LazProject/View/AddNewCardView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct AddNewCardView: View {
     @Environment(\.presentationMode) var presentationMode
     
+    @State private var cardHolder: String = ""
+    @State private var cardNumber: String = ""
+    @State private var cardExp: String = ""
+    @State private var cardCvv: String = ""
+    
     var btnImages = ["mastercard", "paypal", "bank"]
     
     
@@ -20,9 +25,9 @@ struct AddNewCardView: View {
             Color("bgColor")
                 .ignoresSafeArea()
             
-            VStack {
+            VStack(alignment: .leading) {
                 HStack {
-                    ForEach(btnImages, id: \.self) { imageName in 
+                    ForEach(btnImages, id: \.self) { imageName in
                         Button(action: {
                             selectedButton = imageName
                         }) {
@@ -43,7 +48,79 @@ struct AddNewCardView: View {
                         }
                     }
                 }
-                .padding(.top)
+                .padding(.vertical)
+                
+                
+                Text("Cardholder Name")
+                    .font(.custom("Inter-SemiBold", size: 17))
+                    .padding(.leading)
+                
+                TextField("Cardholder Name", text: $cardHolder)
+                    .font(.custom("Inter-Regular", size: 15))
+                    .padding(.leading)
+                    .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
+                    .background(Color("btnColor"))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal)
+                    .autocorrectionDisabled()
+                
+                Text("Card Number")
+                    .font(.custom("Inter-SemiBold", size: 17))
+                    .padding(.leading)
+                
+                TextField("4111 1111 1111 1111", text: $cardNumber)
+                    .font(.custom("Inter-Regular", size: 15))
+                    .padding(.leading)
+                    .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
+                    .background(Color("btnColor"))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal)
+                    .onChange(of: cardNumber) {
+                        if cardNumber.count > 19 {
+                            cardNumber = String(cardNumber.prefix(19))
+                        }
+                        cardNumber = formatCardNumber(cardNumber)
+                    }
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("EXP")
+                            .font(.custom("Inter-SemiBold", size: 17))
+                            .padding(.leading)
+                        
+                        TextField("12/24", text: $cardExp)
+                            .font(.custom("Inter-Regular", size: 15))
+                            .padding(.leading)
+                            .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
+                            .background(Color("btnColor"))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .padding(.horizontal)
+                            .onChange(of: cardExp) {
+                                if cardExp.count > 5 {
+                                    cardExp = String(cardExp.prefix(5))
+                                }
+                                cardExp = formatCardExp(cardExp)
+                            }
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("CVV")
+                            .font(.custom("Inter-SemiBold", size: 17))
+                            .padding(.leading)
+                        
+                        TextField("780", text: $cardCvv)
+                            .font(.custom("Inter-Regular", size: 15))
+                            .padding(.leading)
+                            .frame(maxWidth: .infinity, maxHeight: 50, alignment: .leading)
+                            .background(Color("btnColor"))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .padding(.horizontal)
+                            .onChange(of: cardCvv) {
+                                cardCvv = String(cardCvv.prefix(3))
+                            }
+                    }
+                    
+                }
                 
                 Spacer()
             }
@@ -67,6 +144,33 @@ struct AddNewCardView: View {
             .navigationTitle("Add New Card")
         .navigationBarTitleDisplayMode(.inline)
         }
+    }
+    
+    func formatCardNumber(_ cardNumber: String) -> String {
+        let strippedCardNumber = cardNumber.replacingOccurrences(of: " ", with: "")
+        var formattedCardNumber = ""
+        for (index, character) in strippedCardNumber.enumerated() {
+            if index > 0 && index % 4 == 0 {
+                formattedCardNumber += " "
+            }
+            formattedCardNumber.append(character)
+        }
+        return formattedCardNumber
+    }
+    
+    func formatCardExp(_ cardExp: String) -> String {
+        var formattedCardExp = cardExp
+        if formattedCardExp.count == 3 && !formattedCardExp.contains("/") {
+            let startIndex = formattedCardExp.startIndex
+            let thirdPosition = formattedCardExp.index(startIndex, offsetBy: 2)
+            formattedCardExp.insert("/", at: thirdPosition)
+        }
+        if formattedCardExp.last == "/" {
+            formattedCardExp.removeLast()
+        }
+        formattedCardExp = String(formattedCardExp.prefix(5))
+        
+        return formattedCardExp
     }
 }
 
